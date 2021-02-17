1c14360e3d7eefa72f00522b75b8923913c9bd00
AlexMochalov <AlexMochalov@users.noreply.github.com>
2021-02-17 18:04:03 +0200

    Initial Commit

---
+++ WaveViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaveViewApp
+//
+//  Created by Alex Mochalov on 17/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WaveViewAppTests/WaveViewAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  WaveViewAppTests.swift
+//  WaveViewAppTests
+//
+//  Created by Alex Mochalov on 17/02/2021.
+//
+
+import XCTest
+@testable import WaveViewApp
+
+class WaveViewAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
